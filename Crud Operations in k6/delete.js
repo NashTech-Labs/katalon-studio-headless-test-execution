cf5a763effa52f8b6c1ffdba37124ce8f87de92c
Soniya141 <soniya.raichandani@knoldus.com>
2022-04-29 12:57:45 +0530

    add files

---
+++ Crud Operations in k6/delete.js
@@ -0,0 +1,10 @@
+import { Httpx } from 'https://jslib.k6.io/httpx/0.0.4/index.js';
+
+const session = new Httpx({
+  baseURL: 'https://httpbin.test.k6.io',
+  timeout: 20000, // 20s timeout.
+});
+
+export default function testSuite() {
+  const resp = session.delete(`/delete`);
+}
